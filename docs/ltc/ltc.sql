ba8a71a02988ec0354e5a4aea087939d66af901a
point <alex.softx@gmail.com>
2009-11-11 13:56:01 +0200

    fix: ltc.sql file added

---
+++ docs/ltc/ltc.sql
@@ -0,0 +1,19 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Table structure for table `ltc`
+--
+
+DROP TABLE IF EXISTS `ltc`;
+CREATE TABLE IF NOT EXISTS `ltc` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `language` int(11) NOT NULL,
+  `key` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `value` text COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`,`language`,`key`(255))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+
+SET FOREIGN_KEY_CHECKS=1;
+
