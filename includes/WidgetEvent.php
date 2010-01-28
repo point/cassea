c4933fa3bf7fc83433e8cb5d53030a35f0850087
point <alex.softx@gmail.com>
2010-01-29 01:32:27 +0200

    improve: docs for WidgetEvent were added

--- includes/WidgetEvent.php
+++ includes/WidgetEvent.php
@@ -26,18 +26,70 @@
 * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 }}} -*/
-
-// $Id:$
+/**
+ * This file contains class for sending messages between widgets.
+ *
+ * @author point <alex.softx@gmail.com>
+ * @link http://cassea.wdev.tk/
+ * @version $Id: $
+ * @package system
+ * @since 
+ */
 
 //{{{ WidgetEvent
+/**
+ * This class aggregates all need information for passing messages 
+ * between widgets.
+ * Typical use-case :
+ * <pre><code>
+ * $event = new WidgetEvent("event_name",$id_of_widget);
+ * $event->setParams(array("parameter_name1"=>$parameter_value1));
+ * </code></pre>
+ *
+ * Newly created object could be passed as an argument to the 
+ * <code>notify()</code> method of {@link WidgetEventDispatcher}.
+ *
+ * Besides, it also possible to create event, addressed to all widgets,
+ * registered in the system (broadcast message).
+ */
 class WidgetEvent
 {
 	protected
+		/**
+		 * Name of the event.
+		 * @var string
+		 */
 		$event_name = null,
+		/**
+		 * Id of the source widget
+		 * @var string
+		 */
 		$src_id = null,
+		/**
+		 * Destination id. 
+		 * @var mixed. Could be either string or null for
+		 * broadcast messaging
+		 */
 		$dst_ids = array(),
+		/**
+		 * Parameters, that should be stored in the event.
+		 * @var array
+		 */
 		$event_params = array()
 		;
+
+	//{{{ __construct
+	/**
+	 * Creates new instance of the event.
+	 *
+	 * @param string name of the event
+	 * @param mixed id of the widget, the source of the event. 
+	 * It could be string or null. In last case, receiver-party could not
+	 * determine source of the event
+	 * @param mixed id (or array of ids) of the destination widget. It could be null in order to
+	 * create broadcast message.
+	 * @param array event params. Could be specified later with {@link setParams} method.
+	 */
 	function __construct($event_name,$src_id = null, $dst_ids = null,$event_params = null)
 	{
 		if(isset($event_name))
@@ -51,18 +103,48 @@ class WidgetEvent
 		if(isset($event_params))
 			$this->setParams($event_params);
 	}
+	//}}}
+
+	//{{{ getName
+	/**
+	 * @param null
+	 * @return string the name of the current event
+	 */
 	function getName()
 	{
 		return $this->event_name;
 	}
+	//}}}
+
+	//{{{ setSrc
+	/**
+	 * Defines source widget id.
+	 * @param string id of the source widget
+	 * @return null
+	 */
 	function setSrc($id)
 	{
 		$this->src_id = $id;
 	}
+	//}}}
+
+	//{{{ getSrc
+	/**
+	 * @param null
+	 * @return string id of the source widget
+	 */
 	function getSrc()
 	{
 		return $this->src_id;
 	}
+	//}}}
+
+	//{{{ setDst
+	/**
+	 * @param mixed id of the destination widget. It could be string (single id)
+	 * or array (list of ids).
+	 * @return null
+	 */
 	function setDst($dst)
 	{
 		if(is_string($dst))
@@ -70,24 +152,51 @@ class WidgetEvent
 		elseif(is_array($dst))
 			$this->dst_ids = $dst;
 	}
+	//}}}
+
+	//{{{ idDst
+	/**
+	 * Checks, whenever given id is in the list of recipients.
+	 * @param string id to check
+	 * @return bool
+	 */
 	function inDst($id)
 	{
-		// broadband message
+		// broadcast message
 		if(empty($this->dst_ids)) return true;
 
 		return in_array($id,$this->dst_ids);
 	}
+	//}}}
+
+	//{{{ setParams
+	/**
+	 * Merges current params with the passed array.
+	 *
+	 * @param array of key=>value
+	 * @return null
+	 */
 	function setParams($params)
 	{
 		if(!is_array($params)) return;
 		foreach($params as $k => $v)
 			$this->event_params[$k] = $v;
 	}
+	//}}}
+
+	//{{{ getParam
+	/**
+	 * Retrieves parameter by its name.
+	 *
+	 * @param string name of the parameter
+	 * @return mixed value, stored by {@link setParams}
+	 */
 	function getParam($param_name)
 	{
 		return isset($this->event_params[$param_name])?
 			$this->event_params[$param_name]:null;
 	}
+	//}}}
     //TODO: __get and fluent interfaces
 }
 //}}}
