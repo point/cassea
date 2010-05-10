a973eb0575ad54ae9c45d73f66efea2556039305
point <alex.softx@gmail.com>
2010-05-11 01:36:53 +0300

    fix: docs for Profile were added. Some small fixes

--- includes/user/Profile.php
+++ includes/user/Profile.php
@@ -27,11 +27,69 @@
 * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 }}} -*/
 
+/**
+ * This file contains factory class, that instantiates and returns 
+ * class with profile information for particular user.
+ * 
+ * By default, built-in CasseaProfile is used.
+ *
+ * But this behaviour could be changed with config option
+ * <pre><code>
+ * profile.name = "..."
+ * </code></pre>
+ *
+ * In order to define new profile class, it should be placed to the <code>/vendors/_profile_name_</code> dir and
+ * this name should be pointed in <code>config.ini</code>. 
+ * All profile classes should implement <code>iProfile</code> interface.
+ *
+ * For example, <code>my_profile</code> profile will be looks so:
+ *
+ * <pre><code>
+ * config.ini:
+ * profile.name=my_profile
+ *
+ *
+ * /vendors/my_profile/MyProfile.php with class
+ *
+ * class MyProfile implements iProfile {}
+ *
+ * </code></pre>
+ *
+ * Name of the class will be obtained with <code>nameToClass()</code> function (one of common functions in functios.php).
+ *
+ * @author point <alex.softx@gmail.com>
+ * @link http://cassea.wdev.tk/
+ * @version $Id:$
+ * @package system
+ * @since 
+ */
+
+//{{{ Profile
 class Profile
 {
+	/**
+	 * @var array $loadedProfiles memorize already created objects for given user_id.
+	 */
 	private static $loadedProfiles = array();
+	/**
+	 * @var string $profile_classname memorize obtained from config class name for profile object
+	 */
 	private static $profile_classname = "CasseaProfile";
 
+	//{{{ get
+	/**
+	 * Main function which returns instance of profile class, selected in config file.
+	 *
+	 * <code>$user_id</code> parameter is used to define which info shoud contain profile object.
+	 * If this parameter is omitted, profile for current user will be returned.
+	 *
+	 * For performance reasons, objects are saved to reduce DB queries. If calling this method 
+	 * not for the first time, previously created object will be returned.
+	 *
+	 * @param mixed $user_id it could be either null or int with id of user to retrieve data.
+	 * @return iProfile initialized object of profile class.
+	 * @throws ProfileException if class isn't impements iProfile interface.
+	 */
 	static function get($user_id = null)
 	{
 		if($user_id === null)
@@ -44,24 +102,49 @@ class Profile
 			throw new ProfileException("Profile must be instance of iProfile interface");
 		return self::$loadedProfiles[$user_id] = $o;
 	}
+	//}}}
 
+	//{{{ addUser
+	/**
+	 * This method called when new user is creating. 
+	 * <code>addUser</code> is calling statically because there is no profile for 
+	 * new user and we couldn't call {@link get} before.
+	 *
+	 * This method simply proxies call to the specified class.
+	 *
+	 * @param int $user_id user id of the newly created member.
+	 * @param mixed $param could be either null or array with specific parameters.
+	 * @return null
+	 */
 	static function addUser($user_id, $param=null){
 		return call_user_func( self::getProfileClass().'::addUser',$user_id, $param);
 	}
+	//}}}
 
+	//{{{ getProfileClass
+	/**
+	 * Tries to calculate name of the class from config and add to vendor lookup
+	 * paths list retrieved directory.
+	 *
+	 * For performance reasons, obtained classname is memerozed in internal private variable.
+	 *
+	 * @param null
+	 * @return string class name of the profile.
+	 */
 	static private function getProfileClass(){
 		if(self::$profile_classname) return self::$profile_classname;
 
 		try {
-			self::$profile_classname = Config::getInstance()->profile->name;
+			if($_t = Config::getInstance()->profile->name != "cassea") //for compatibility
+				self::$profile_classname = $_t;
 		}
 		catch(ConfigException $e){
 			//just normal. Using default profile class
 		}
 
-		self::$profile_classname = nameToClass($profile_classname);
 		Autoload::addVendor(self::$profile_classname);
-		return self::$profile_classname;
+		return self::$profile_classname = nameToClass($profile_classname);
 	}
+	//}}}
 }
 
